c644f6e9de1d998a900fc23b03e49d15d47d9192
Kevindeving <98kevchen@gmail.com>
2014-03-14 14:21:02 +0100

    require non-opensource files

--- e975.mk
+++ e975.mk
@@ -32,4 +32,4 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/e975/device.mk)
-$(call inherit-product-if-exists, vendor/lge/e975/e975-vendor.mk)
+$(call inherit-product, vendor/lge/e975/e975-vendor.mk)
