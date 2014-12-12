925f96ddcde485a4aa9c77922599f652602fe857
Kevin <98kevchen@gmail.com>
2014-12-12 22:48:41 +0100

    board: add many possible asserts for optimus g

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,3 +53,6 @@ BOARD_SEPOLICY_DIRS += \
 #TARGET_QCOM_AUDIO_VARIANT := caf
 
 BOARD_HARDWARE_CLASS += device/lge/e975/cmhw
+
+#Asserts
+TARGET_OTA_ASSERT_DEVICE := gee,geeb,geehrc,e975,geehrc_4g_spr,geespr,ls970,geehrc4g,geehrc4g_spr_us,geebus,e970,e973,e971,geeb_att_us
